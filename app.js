8bd9a249c859ea11f4e665d1a71b28cad4f82658
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-06-20 22:19:59 +0530

    Handling Different Routes

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 
 const express=require('express');
+const bodyParse=require('body-parser');
 const app=express();
-app.use((req,res,next)=>{
-    console.log('in middleware');
-    next();
+
+app.use(bodyParse.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="Quantity"><button type="submit">Add Product</button></form>')
 });
-app.use((req,res,next)=>{
-    console.log('in another middleware');
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+
     res.send('<h1>Welcome to Express.js</h1>');
     
 });
