3ffecd214a5aad8e1faa0f33337109860fc4f751
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-06-23 13:15:58 +0530

    main first page

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const app=express();
 
 const adminRoutes=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
+const contactRoutes=require('./routes/contact');
+const successRoutes=require('./routes/success');
+const path=require('path');
 
 app.use(bodyParse.urlencoded({extended:false}));
 
 app.use('/admin',adminRoutes);
 app.use('/shop',shopRoutes);
-
+app.use('/admin',contactRoutes);
+app.use(successRoutes);
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>ERROR:404-Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 app.listen(4000);
