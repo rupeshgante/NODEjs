df8904bae37560241dba3141b89d6d78e426269d
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-06-23 13:16:49 +0530

    shopping products js file

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
 const express=require('express');
 const router=express.Router();
+const path=require('path');
+const rootDir=require('../util/path')
 
 router.get('/',(req,res,next)=>{
 
-    res.send('<h1>Welcome to Express.js</h1>');
+    res.sendFile(path.join(rootDir,'views','shop.html'));
     
 });
 
