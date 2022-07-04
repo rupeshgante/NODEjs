8b3d0cf9ebb541df4ab7c4ab2f70bcdaeed64868
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-07-04 13:04:04 +0530

    added assoiation of user and products

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const ejs=require('ejs');
-const errorController=require('./controllers/error');
+
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -11,16 +15,44 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const contactRoutes=require('./routes/contact');
-const successRoutes=require('./routes/success');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use('/admin',contactRoutes);
-app.use(successRoutes);
 
 app.use(errorController.get404);
 
-app.listen(4000);
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'rupesh', email: 'rupesh@gmail.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    app.listen(4000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
