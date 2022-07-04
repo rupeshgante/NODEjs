65f86464709afe87f0501c64586ff4f1af3bd246
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-07-04 14:51:23 +0530

    added cart

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart=require('./models/cart');
+const CartItem=require('./models/cart-item');
+
 
 const app = express();
 
@@ -35,6 +38,11 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through:CartItem});
+Product.belongsToMany(Cart,{through:CartItem});
+
 
 sequelize
   // .sync({ force: true })
@@ -51,7 +59,11 @@ sequelize
   })
   .then(user => {
     // console.log(user);
+   return user.createCart();
+  })
+  .then(cart=>{
     app.listen(4000);
+
   })
   .catch(err => {
     console.log(err);
